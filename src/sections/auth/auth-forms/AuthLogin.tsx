033e28770ed3f2f4f87ed573afb25b0c447e09e4
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2024-02-18 03:16:22 -0500

    update instance of axios

--- src/sections/auth/auth-forms/AuthLogin.tsx
+++ src/sections/auth/auth-forms/AuthLogin.tsx
@@ -17,6 +17,7 @@ import PasswordInput from "components/Input/PasswordInput";
 import { isEmpty } from "utils/helpers/main";
 import ErrorHandler from "components/ErrorHandler";
 import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from "config";
+import axios from "axios";
 
 const validationSchema = Yup.object().shape({
   NAt_ID: Yup.string().required(),
@@ -46,13 +47,13 @@ const AuthLogin = () => {
 
   const onSubmit = async (data: LoginRequest) => {
     try {
-      // await login(data);
-      fetch("https://deepdev.ir/Projects/KomailClinic/Admin/Login_Super.php", {
-        method: "post",
-        body: JSON.stringify(data),
-      }).then((res) => {
-        console.log(res);
-      });
+      await login(data);
+      // fetch("https://deepdev.ir/Projects/KomailClinic/Admin/Login_Super.php", {
+      //   method: "post",
+      //   body: JSON.stringify(data),
+      // }).then((res) => {
+      //   console.log(res);
+      // });
     } catch (err: any) {
       setError(err);
     }
